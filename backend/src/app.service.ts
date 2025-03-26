ad060b3336a54ec2284f2b796af267e27b5f7d4f
Marllon Ramos <marllonsteam@gmail.com>
2025-03-26 16:25:17 -0300

    Fix - Eslint rules & prettier ; Feat/+Prisma

--- backend/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { x } from '@core';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return `Olá! Bem-vindo ao meu serviço NestJS! -- ${x}`;
-  }
-}
